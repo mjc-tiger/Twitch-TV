990ca18dba793975cc344f65eac9e8599013ab74
mjc-tiger <mj.cupial@gmail.com>
2017-09-02 23:49:08 +0200

    taking data with error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //create variables
-  var followers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin"];
+  var followers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin","kupłaqó"];
   // get URL for fCC
   var url = "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp";
   //check status ONLINE/OFFLINE
@@ -11,46 +11,74 @@ $(document).ready(function() {
       $("#status").html("ONLINE");
     }
   });
-  //check followers for fCC
+
+  // --------------- NEW CODE -----------------------------------
   var urlFollower = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp/follows/channels";
   $.getJSON(urlFollower, function(data2) {
     for (var i = 0; i < data2.follows.length; i++) {
       var name = data2.follows[i].channel.display_name;
-      var logo = data2.follows[i].channel.logo;
       followers.push(name);
     }
-    for (var i = 0; i < followers.length; i++) {
-      $(".main-twitchTV").append("<h2>" + followers[i] + "</h2>");
+    console.log(followers);
+    for (var i=0; i<followers.length; i++){
+      var url2 = "https://wind-bow.glitch.me/twitch-api/streams/"+followers[i]+"/?callback=?";
+      $.getJSON(url2).done(function(data3){
+        var logo;
+        var status;
+        var name;
+        if (data3.error){
+          logo = "https://vignette1.wikia.nocookie.net/nationlife/images/c/c0/No-sign.png/revision/latest?cb=20110628223434";
+          name = data3.message;
+          status = data3.error;
+          
+
+          $(".main-twitchTV").prepend("<div class='main-twitchTV'>"+
+          "<div class='mtTV-left'>"+
+            "<img src='"+logo+"'>"+
+            "<h2>"+name+"</h2>"+
+            "<p>"+status+"</p>"+
+          "</div>"+
+          "<div class='mtTV-right'>"+
+            "<h2>"+status+"</h2>"+
+          "</div>"+
+          "<div style='clear:both;'></div><hr></div>");
+        }
+        console.log(data3);
+      });
     }
 
+  // --------------- THE END OF NEW CODE ------------------------
 
 
-    // for (var i=0;i<followers.length;i++){
-    //   urlStatus = "https://wind-bow.glitch.me/twitch-api/streams/"+followers[i];
-    //   $.getJSON(urlStatus, function(data3){
-    //     if (data3.stream != null){
-    //       var logo = data3.stream.channel.logo;
-    //       var name = data3.stream.channel.display_name;
-    //       var status = data3.stream.channel.status;
-    //       $(".mtTV-right").prepend("<img src='"+logo+"'><h2>"+name+"</h2>");
-    //     } else {
-    //       var offLogo = "https://cdn3.iconfinder.com/data/icons/wifi-2/460/no-conection-512.png";
-    //       $(".mtTV-right").append("<img src='"+offLogo+"'>");
-    //     }
-    //   })
-    // }
 
   });
+
+  // Initialize the scroll button
   $("#scroll-btn").click(function() {
     $("body").animate({
       scrollTop: 0
     }, "slow");
     return false;
   });
-  $(window).scroll(function(){
-    if($(this).scrollTop()>300) {$("#scroll-btn").fadeIn();}
-    else {$("#scroll-btn").fadeOut();}
+
+  // Show or hide scroll button function
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $("#scroll-btn").fadeIn();
+    } else {
+      $("#scroll-btn").fadeOut();
+    }
+
   });
+
+  // fix the details container when scrolled
+  $(window).scroll(function () {
+            if ($(window).scrollTop() > 300) {
+                $(".details").addClass("fixed");
+            } else {
+                $(".details").removeClass("fixed");
+            }
+        });
 
 
 });
