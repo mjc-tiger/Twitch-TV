c92b5948c41d378f70f4e88efef8078672fe05b3
mjc-tiger <mj.cupial@gmail.com>
2017-09-03 13:19:22 +0200

    shorter code with else-if statement

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
 $(document).ready(function() {
-  //create variables
-  var followers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin","kupłaqó"];
-  // get URL for fCC
-  var url = "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp";
-  //check status ONLINE/OFFLINE
-  $.getJSON(url, function(data) {
-    if (data.stream === null) {
-      $("#status").html("OFFLINE");
-    } else {
-      $("#status").html("ONLINE");
-    }
-  });
+  //An array from "Use the Twitchtv JSON API project (fCC)
+  var followers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin","kółeczko"];
 
-  // --------------- NEW CODE -----------------------------------
-  var urlFollower = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp/follows/channels";
-  $.getJSON(urlFollower, function(data2) {
-    for (var i = 0; i < data2.follows.length; i++) {
-      var name = data2.follows[i].channel.display_name;
+  //aditional Twitchtv users - fCC followers
+  var followersURL = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp/follows/channels";
+  $.getJSON(followersURL, function(data) {
+    for (let i = 0; i < data.follows.length; i++) {
+      var name = data.follows[i].channel.display_name;
       followers.push(name);
     }
     console.log(followers);
-    for (var i=0; i<followers.length; i++){
-      var url2 = "https://wind-bow.glitch.me/twitch-api/streams/"+followers[i]+"/?callback=?";
-      $.getJSON(url2).done(function(data3){
-        var logo;
-        var status;
-        var name;
-        if (data3.error){
-          logo = "https://vignette1.wikia.nocookie.net/nationlife/images/c/c0/No-sign.png/revision/latest?cb=20110628223434";
-          name = data3.message;
-          status = data3.error;
-          
-
-          $(".main-twitchTV").prepend("<div class='main-twitchTV'>"+
-          "<div class='mtTV-left'>"+
-            "<img src='"+logo+"'>"+
-            "<h2>"+name+"</h2>"+
-            "<p>"+status+"</p>"+
-          "</div>"+
-          "<div class='mtTV-right'>"+
-            "<h2>"+status+"</h2>"+
-          "</div>"+
-          "<div style='clear:both;'></div><hr></div>");
-        }
-        console.log(data3);
+    for (let i=0; i<followers.length; i++){
+      var streamURL = 'https://wind-bow.gomix.me/twitch-api/streams/' + followers[i] + '?callback=?';
+      $.getJSON(streamURL, function(data2){
+        var game, status;
+        if (data2.stream === null){
+          game ="Offline";
+          status = "offline";
+        } else if (data2.stream === undefined){
+          game = "Account closed or does not exist";
+          status = "offline";
+        } else {
+          game = data2.stream.game;
+          status = "online";
+        };
+        console.log(game);
       });
-    }
+      
 
-  // --------------- THE END OF NEW CODE ------------------------
+    };
 
 
 
+
+
+    
   });
+    
+
+
+
+    
+          
+          // $(".main-twitchTV").prepend("<div class='main-twitchTV'>"+
+          // "<div class='mtTV-left'>"+
+          //   "<img src='"+logo+"'>"+
+          //   "<h2>"+name+"</h2>"+
+          //   "<p>"+status+"</p>"+
+          // "</div>"+
+          // "<div class='mtTV-right'>"+
+          //   "<h2>OFFLINE</h2>"+
+          // "</div>"+
+          // "<div style='clear:both;'></div><hr></div>");
+        
+        // console.log(data3);
+  
 
   // Initialize the scroll button
   $("#scroll-btn").click(function() {
